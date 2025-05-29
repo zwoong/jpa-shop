31522c267f00bc66144c778a590bc3f28c4a840f
zwoong <zwoong6867@gmail.com>
2025-05-29 20:35:24 +0900

    ✨ 엔티티 클래스 개발1

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+  @Id @GeneratedValue
+  @Column(name = "member_id")
+  private Long id;
+
+  private String name;
+
+  @Embedded
+  private Address address;
+
+  @OneToMany(mappedBy = "member")
+  private List<Order> orders = new ArrayList<>();
+}
---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+  @Id @GeneratedValue
+  @Column(name = "order_id")
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @OneToMany(mappedBy = "order")
+  private List<OrderItem> orderItems = new ArrayList<>();
+
+  @OneToOne
+  @JoinColumn(name = "delivery_id")
+  private Delivery delivery;
+
+  private LocalDateTime orderDate; // 주문시간
+
+  @Enumerated(EnumType.STRING)
+  private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+}
