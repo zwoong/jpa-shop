be809f03cff1c2c331f071d2f0bc4f3f7310099b
zwoong <zwoong6867@gmail.com>
2025-05-30 16:22:48 +0900

    ✨ 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -10,6 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 @Entity
 @Getter @Setter
@@ -29,4 +30,24 @@ public class OrderItem {
 
   private int orderPrice; // 주문 가격
   private int count; // 주문 수량
+
+  //==생성 메서드==//
+  public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+    OrderItem orderItem = new OrderItem();
+    orderItem.setItem(item);
+    orderItem.setOrderPrice(orderPrice);
+    orderItem.setCount(count);
+
+    item.removeStock(count);
+    return orderItem;
+  }
+
+  //==비즈니스 로직==//
+  public void cancel() {
+    this.getItem().addStock(count);
+  }
+
+  public int getTotalPrice() {
+    return this.getOrderPrice() * this.getCount();
+  }
 }
