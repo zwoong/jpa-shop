e0c28dec726405bd8a0e3bef1a4f591c57c0f929
zwoong <zwoong6867@gmail.com>
2025-06-06 13:27:34 +0900

    ✨ 회원 등록 API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -19,6 +20,7 @@ public class Member {
   @Column(name = "member_id")
   private Long id;
 
+  @NotEmpty
   private String name;
 
   @Embedded
