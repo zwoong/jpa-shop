9ba7bca92add62fe17d673c9c6846f503bbc11cb
zwoong <zwoong6867@gmail.com>
2025-06-06 11:01:59 +0900

    ✨ 변경 감지와 병합

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -40,5 +42,28 @@ public class ItemController {
     List<Item> items = itemService.findItems();
     model.addAttribute("items", items);
     return "items/itemList";
+  }
+
+  @GetMapping("items/{itemId}edit")
+  public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+    Book item = (Book) itemService.findOne(itemId);
+
+    BookForm form = new BookForm();
+    form.setId(item.getId());
+    form.setName(item.getName());
+    form.setPrice(item.getPrice());
+    form.setStockQuantity(item.getStockQuantity());
+    form.setAuthor(item.getAuthor());
+    form.setIsbn(item.getIsbn());
+
+    model.addAttribute("form", form);
+    return "items/updateItemForm";
+  }
+
+  @PostMapping("items/{itemId}edit")
+  public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+
+    itemService.updateItem(form.getId(), form.getName(), form.getPrice(), form.getStockQuantity());
+    return "redirect:items";
   }
 }
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import java.util.List;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,14 @@ public class ItemService {
   @Transactional
   public void saveItem(Item item) {
     itemRepository.save(item);
+  }
+
+  @Transactional
+  public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+    Item findItem = itemRepository.findOne(itemId);
+    findItem.setPrice(price);
+    findItem.setName(name);
+    findItem.setStockQuantity(stockQuantity);
   }
 
   public List<Item> findItems() {
